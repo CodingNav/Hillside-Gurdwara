39cd9631abf3380217bb28db7ce7a0a168282dba
CodingNav <navleenkaurr@gmail.com>
2021-08-10 12:15:59 -0400

    Created fetch

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,47 @@
 $(document).foundation();
+
+$.ajax({
+    method: "POST",
+    url: "https://www.sikhcoalition.org/wp-json/facetwp/v1/refresh",
+    data: {
+        "action": "facetwp_refresh",
+        "data": {
+            "facets": {
+                "resource_types": [
+                    "community-resources"
+                ],
+                "keyword": "",
+                "document_type": [],
+                "topic": [],
+                "issue_area": [],
+                "audience": [],
+                "language": [],
+                "geography": [],
+                "date_range": []
+            },
+            "frozen_facets": {},
+            "http_params": {
+                "get": {
+                    "fwp_resource_types": "community-resources"
+                },
+                "uri": "resources",
+                "url_vars": {
+                    "resource_types": [
+                        "community-resources"
+                    ]
+                }
+            },
+            "template": "resources",
+            "extras": {
+                "pager": true,
+                "sort": "default"
+            },
+            "soft_refresh": 0,
+            "is_bfcache": 1,
+            "first_load": 0,
+            "paged": 1
+        }
+    }
+}).then(function(response) {
+    console.log(response);
+})
