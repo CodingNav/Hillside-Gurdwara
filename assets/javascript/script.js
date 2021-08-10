117bd73914bbe7d853b2be3647781c4153f30d27
CodingNav <navleenkaurr@gmail.com>
2021-08-10 13:10:23 -0400

    Changed fetch request

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,51 +1,18 @@
 $(document).foundation();
 
-$.ajax({
-    method: "POST",
-    url: "https://www.sikhcoalition.org/wp-json/facetwp/v1/refresh",
-    headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Request-Headers": "x-requested-with"
-    },
-    data: JSON.stringify({
-        "action": "facetwp_refresh",
-        "data": {
-            "facets": {
-                "resource_types": [
-                    "community-resources"
-                ],
-                "keyword": "",
-                "document_type": [],
-                "topic": [],
-                "issue_area": [],
-                "audience": [],
-                "language": [],
-                "geography": [],
-                "date_range": []
-            },
-            "frozen_facets": {},
-            "http_params": {
-                "get": {
-                    "fwp_resource_types": "community-resources"
-                },
-                "uri": "resources",
-                "url_vars": {
-                    "resource_types": [
-                        "community-resources"
-                    ]
-                }
-            },
-            "template": "resources",
-            "extras": {
-                "pager": true,
-                "sort": "default"
-            },
-            "soft_refresh": 0,
-            "is_bfcache": 1,
-            "first_load": 0,
-            "paged": 1
-        }
-    })
-}).then(function(response) {
-    console.log(response);
-})
+function getResources(resourceType) {
+const proxy = "https://api.allorigins.win/get?url=";
+const resourcesURL = proxy + encodeURIComponent("https://www.sikhcoalition.org/resources/?fwp_resource_types=" + resourceType);
+
+const settings = {
+    "url": resourcesURL,
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    const resourcesPage = $(response.contents);
+    resourcesPage.find("article");
+
+  });
+};
