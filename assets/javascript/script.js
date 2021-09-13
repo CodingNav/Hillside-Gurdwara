1466bdc2eca0cf9623d5faf54abe2cbc418ca33b
CodingNav <navleenkaurr@gmail.com>
2021-09-13 19:05:44 -0400

    Set up contact form and changed css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,15 @@
 $(document).foundation();
 
 function getResources(resourceType) {
-const proxy = "https://api.allorigins.win/get?url=";
-const resourcesURL = proxy + encodeURIComponent("https://www.sikhcoalition.org/resources/?fwp_resource_types=" + resourceType);
+  const proxy = "https://api.allorigins.win/get?url=";
+  const resourcesURL = proxy + encodeURIComponent("https://www.sikhcoalition.org/resources/?fwp_resource_types=" + resourceType);
 
-const settings = {
+  const settings = {
     "url": resourcesURL,
     "method": "GET",
     "timeout": 0,
   };
-  
+
   $.ajax(settings).done(function (response) {
     const resourcesPage = $(response.contents);
     resourcesPage.find("article");
@@ -20,8 +20,46 @@ const settings = {
 
 // Scroller.scrollVerticalToElementById("about-section", -50);
 
-$(".top-bar a, footer a").on("click", function(e) {
+$(".top-bar a, footer a").on("click", function (e) {
   e.preventDefault();
   const height = $(".top-bar").height();
   Scroller.scrollVerticalToElementById(this.href.split("#")[1], height);
+});
+
+
+
+
+var contactForm = document.querySelector("#contact-section form");
+
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  var nameInput = document.querySelector("#name-input");
+  var emailInput = document.querySelector("#email-input");
+  var numberInput = document.querySelector("#number-input");
+  var subjectInput = document.querySelector("#subject-input");
+  var messageInput = document.querySelector("#message-input");
+  var submitButton = document.querySelector("#submit-btn");
+
+  submitButton.disabled = true;
+
+  var emailData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    number: numberInput.value,
+    subject: subjectInput.value,
+    message: messageInput.value
+  }
+
+  fetch('https://hillside-gurdwara-email.herokuapp.com/api/send-email', {
+    method: 'post',
+    body: JSON.stringify(emailData),
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    }
+  }).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    contactForm.innerHTML = `<p class="text-center">Thank you for submitting.<br>We will be in contact with you soon.</p>`
+  });
 });
